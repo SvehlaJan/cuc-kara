0bef06cd0965f1612c16e23f7f70391c7719e330
Ján Švehla <jan.svehla@avast.com>
2022-04-10 08:08:44 +0200

    Update

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,8 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
       ? '/cuc-kara/'
-      : '/'
+      : '/',
+  basePath: process.env.NODE_ENV === 'production'
+      ? '/cuc-kara/'
+      : '/',
 })
